0a20e43f70ae3c9a6a20ec5dd9005d2640aeff29
Bryan Alves <info.bryanalves@gmail.com>
2025-08-26 00:48:26 -0300

    feat: Created Menu component

---
+++ resources/js/components/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="menu">
+    <slot />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.menu {
+  background: #19b2b2c2;
+  width: 100%;
+  max-height: 100vh;
+  max-width: 365px;
+  padding: 10px;
+  overflow-y: auto;
+}
+</style>
