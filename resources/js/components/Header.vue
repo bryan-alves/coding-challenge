2f545d930dedbab70c0d435cb2ee7a73fbd3aa18
Bryan Alves <info.bryanalves@gmail.com>
2025-08-26 00:47:39 -0300

    feat: Created Header component

---
+++ resources/js/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <img src="https://pipelead.to/wp-content/uploads/2023/10/logo-pipeleads.svg" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  align-items: center;
+  height: 80px;
+  width: 100%;
+  background: #f5f5f5;
+  padding: 0 30px;
+}
+</style>
