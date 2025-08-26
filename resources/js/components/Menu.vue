5357e18c4546c04a50c951a7962d9aa916564cf4
Bryan Alves <info.bryanalves@gmail.com>
2025-08-26 14:56:21 -0300

    feat: Added custom scrollbar to menu

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -9,8 +9,26 @@
   background: #19b2b2c2;
   width: 100%;
   max-height: 100vh;
+  min-height: calc(100vh - 80px);
   max-width: 365px;
   padding: 10px;
   overflow-y: auto;
+}
+
+.menu::-webkit-scrollbar {
+  width: 10px;
+}
+
+.menu::-webkit-scrollbar-track {
+  background: #19b2b2c2;
+}
+
+.menu::-webkit-scrollbar-thumb {
+  background: #4d4d4d;
+  border-radius: 5px;
+}
+
+.menu::-webkit-scrollbar-thumb:hover {
+  background: #363636;
 }
 </style>
