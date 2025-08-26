3f0dd67d038579b8451b3567974db1143d1d56f0
Bryan Alves <info.bryanalves@gmail.com>
2025-08-26 00:48:01 -0300

    feat: Created Container component

---
+++ resources/js/components/Container.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.container {
+  background: #1494941c;
+  width: 100%;
+  max-width: 100% !important;
+  display: flex;
+}
+</style>
