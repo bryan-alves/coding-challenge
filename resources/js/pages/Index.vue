3191bcf27f757229d3c4ec5d7068ef9cda5645d9
Bryan Alves <info.bryanalves@gmail.com>
2025-08-29 20:44:47 -0300

    fix: Fixed component path to import

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -4,7 +4,7 @@ import Sidebar from "@/components/layouts/Sidebar.vue";
 import Container from "@/components/layouts/Container.vue";
 import Channels from "@/components/layouts/Channels.vue";
 import Contacts from "@/components/layouts/Contacts.vue";
-import MessageModal from "@/components/MessageModal.vue"
+import MessageModal from "@/components/ui/MessageModal.vue"
 import Content from "@/components/layouts/Content.vue";
 
 import { useGlobalStore } from "@/stores/global";
