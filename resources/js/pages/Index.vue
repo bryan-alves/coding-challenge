d7bc658014e433b37037bb88f212a54fb96e98cb
Bryan Alves <info.bryanalves@gmail.com>
2025-08-31 14:48:29 -0300

    feat: Load channel in query params

--- resources/js/pages/Index.vue
+++ resources/js/pages/Index.vue
@@ -1,37 +1,51 @@
 <script setup>
-import { Head } from '@inertiajs/inertia-vue3';
+import { Head } from "@inertiajs/inertia-vue3";
 import Header from "@/components/layouts/Header.vue";
 import Sidebar from "@/components/layouts/Sidebar.vue";
 import Container from "@/components/layouts/Container.vue";
 import Channels from "@/components/layouts/Channels.vue";
 import Contacts from "@/components/layouts/Contacts.vue";
-import MessageModal from "@/components/ui/MessageModal.vue"
+import MessageModal from "@/components/ui/MessageModal.vue";
 import Content from "@/components/layouts/Content.vue";
 
 import { useGlobalStore } from "@/stores/global";
+import { onMounted } from "vue";
 
 const props = defineProps({
   contacts: Array,
   channels: Array,
+  modalContacts: Array,
 });
 
 const store = useGlobalStore();
+
+onMounted(() => {
+  const params = new URLSearchParams(window.location.search);
+  const channel = params.get("channel") || "all";
+
+  store.changeChannel(channel);
+});
 </script>
 
 <template>
   <div>
     <Head>
-      <title>{{ 'Pipechat' }}</title>
+      <title>{{ "Pipechat" }}</title>
       <link rel="icon" href="/images/logo.png" />
     </Head>
     <Container>
       <Sidebar>
         <Header />
-        <Channels :channels="channels" @openModal="store.toggleNewMessageModal(true)"/>
+        <Channels :channels="channels" @openModal="store.toggleNewMessageModal(true)" />
         <Contacts :contacts="contacts" />
       </Sidebar>
       <Content />
-      <MessageModal v-if="store.newMessageModal" :channels="channels" @close="store.toggleNewMessageModal(false)"/>
+      <MessageModal
+        v-if="store.newMessageModal"
+        :channels="channels"
+        @close="store.toggleNewMessageModal(false)"
+        :modalContacts="modalContacts"
+      />
     </Container>
   </div>
 </template>
