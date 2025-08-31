f62b28b4a5c60f53a280ecb0d5d6c0fac675dd5b
Bryan Alves <info.bryanalves@gmail.com>
2025-08-30 22:17:29 -0300

    refactor: Added try catch and logs

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -14,62 +14,91 @@ class ChatController extends Controller
 {
     public function index(Request $request)
     {
-        $channels = Channel::pluck('name')->toArray();
+        try {
+            $channels = Channel::pluck('name')->toArray();
 
-        $contactsQuery = Contact::with(['channel', 'lastMessage'])
-            ->withCount([
-                'unreadMessages as unread_messages_count' => function ($query) {
-                    $query->where('origin', 'received');
+            $contactsQuery = Contact::with(['channel', 'lastMessage'])
+                ->withCount([
+                    'unreadMessages as unread_messages_count' => function ($query) {
+                        $query->where('origin', 'received');
+                    }
+                ]);
+
+            $selectedChannel = $request->query('channel', 'all');
+
+            if ($selectedChannel !== 'all') {
+                $channelId = Channel::where('name', $selectedChannel)->value('id');
+
+                if ($channelId) {
+                    $contactsQuery->where('channel_id', $channelId);
                 }
-            ]);
-
-        $selectedChannel = $request->query('channel', 'all');
-
-        if ($selectedChannel !== 'all') {
-            $channelId = Channel::where('name', $selectedChannel)->value('id');
-
-            if ($channelId) {
-                $contactsQuery->where('channel_id', $channelId);
             }
+
+            $contacts = $contactsQuery->get();
+
+            $messages = [];
+
+            if ($request->contact_id) {
+                $messages = Message::where('contact_id', $request->contact_id)
+                ->orderBy('id', 'desc')
+                ->paginate(20);
+            }
+
+            return Inertia::render('Index', [
+                'contacts' => $contacts,
+                'channels' => $channels,
+                'selectedChannel' => $selectedChannel,
+                'messages' => $messages
+            ]);
+        } catch(Exception $e) {
+            \Log::error("Erro ler mensagem", ['exception' => $e]);
+
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], 500);
         }
-
-        $contacts = $contactsQuery->get();
-
-        $messages = [];
-
-        if ($request->contact_id) {
-            $messages = Message::where('contact_id', $request->contact_id)
-            ->orderBy('id', 'desc')
-            ->paginate(20);
-        }
-
-        return Inertia::render('Index', [
-            'contacts' => $contacts,
-            'channels' => $channels,
-            'selectedChannel' => $selectedChannel,
-            'messages' => $messages
-        ]);
     }
 
     public function readMessage(Request $request)
     {
-        Message::where('contact_id', $request->contact_id)
-            ->where('is_read', false)
-            ->update(['is_read' => true]);
+        try {
+            \Log::info("Mensagem lida", [$request->all()]);
+
+            Message::where('contact_id', $request->contact_id)
+                ->where('is_read', false)
+                ->update(['is_read' => true]);
 
             return $this->index($request);
+        } catch(Exception $e) {
+            \Log::error("Erro ler mensagem", ['exception' => $e]);
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function sendMessage(Request $request)
     {
-        Message::create([
-            'user_id'    => 1,
-            'contact_id' => $request->contact_id,
-            'message'    => $request->content,
-            'origin'     => 'sent',
-            'is_read'    => false,
-        ]);
+        try {
+            \Log::info("Mensagem enviada", [$request->all()]);
 
-        return $this->index($request);
+            Message::create([
+                'user_id'    => 1,
+                'contact_id' => $request->contact_id,
+                'message'    => $request->content,
+                'origin'     => 'sent',
+                'is_read'    => false,
+            ]);
+
+            return $this->index($request);
+        } catch(Exception $e) {
+            \Log::error("Erro ao enviar mensagem", ['exception' => $e]);
+            return response()->json([
+                'success' => false,
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 }
