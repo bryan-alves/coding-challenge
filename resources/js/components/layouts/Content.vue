a38789095e43872bdf4c8f0014bc471e0a38a059
Bryan Alves <info.bryanalves@gmail.com>
2025-08-30 21:51:34 -0300

    feat: Added close button

--- resources/js/components/layouts/Content.vue
+++ resources/js/components/layouts/Content.vue
@@ -1,22 +1,34 @@
 <script setup>
 import { useGlobalStore } from "@/stores/global";
+import { Icon } from "@iconify/vue";
 import Chat from "../layouts/Chat.vue";
 import ThemeToggle from "../ui/ThemeToggle.vue";
 
 const store = useGlobalStore();
+
+function closeChat() {
+  store.changeContact(false);
+}
 </script>
 
 <template>
   <div class="content">
     <ThemeToggle />
-    <div v-if="store.selectedContact" style="height: 100%">
+    <div title="Fechar" @click="closeChat">
+      <Icon
+        v-if="store.selectedContact"
+        class="content__close"
+        :icon="'carbon:close-outline'"
+        width="34"
+        height="34"
+        color="#df4d4d"
+      />
+    </div>
+    <div v-if="store.selectedContact" style="height: 100%" @keyup.esc="closeChat">
       <Chat />
     </div>
     <div class="content__empty" v-else>
-      <img
-        :src="`/images/logo-${store.theme}.svg`"
-        alt=""
-      />
+      <img :src="`/images/logo-${store.theme}.svg`" alt="" />
     </div>
   </div>
 </template>
@@ -26,6 +38,13 @@ const store = useGlobalStore();
   width: 100%;
   position: relative;
   background: var(--content-background-color);
+
+  &__close {
+    position: absolute;
+    right: 100px;
+    top: 24px;
+    cursor: pointer;
+  }
 
   &__empty {
     height: 100%;
